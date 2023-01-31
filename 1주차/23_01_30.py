ca83a8da438e565b82463e27c23cc3d69781beea
김웅서 <ungseo@gmail.com>
2023-01-31 22:33:47 +0900

    1/31문풀

--- 1주차/23_01_30.py
+++ 1주차/23_01_30.py
@@ -1,45 +1,45 @@
-# ##### 1. 두수의 합
-# ```` python
-# def solution(num1, num2):
-#     answer = num1 + num2
-#     return answer
+##### 1. 두수의 합
 
-# ##### 2. 두수의 차
-# def solution(num1, num2):
-#     answer = num1 - num2
-#     return answer
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
 
-# ##### 3. 두수의 곱
-# def solution(num1, num2):
-#     answer = num1 * num2
-#     return answer
+##### 2. 두수의 차
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
 
-# ##### 4. 몫 구하기
-# def solution(num1, num2):
-#     answer = num1 // num2
-#     return answer
+##### 3. 두수의 곱
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
 
-# ##### 5. 두 수의 나눗셈
-# def solution(num1, num2):
-#     answer = (num1 / num2) * 1000
-#     return int(answer)
+##### 4. 몫 구하기
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
 
-# ##### 6. 숫자 비교하기
-# def solution(num1, num2):
-#     flag = num1 == num2
-#     if flag:
-#         return 1
-#     else:
-#         return -1
+##### 5. 두 수의 나눗셈
+def solution(num1, num2):
+    answer = (num1 / num2) * 1000
+    return int(answer)
 
-# ##### 7. 분수의 덧셈
-# def solution(numer1, denom1, numer2, denom2):
-#     import math
+##### 6. 숫자 비교하기
+def solution(num1, num2):
+    flag = num1 == num2
+    if flag:
+        return 1
+    else:
+        return -1
+
+##### 7. 분수의 덧셈
+def solution(numer1, denom1, numer2, denom2):
+    import math
     
-#     lcm = math.lcm(denom1,denom2)
-#     numer1 *= int(lcm/denom1) 
-#     numer2 *= int(lcm/denom2)
-#     return [numer1+numer2,lcm]
+    lcm = math.lcm(denom1,denom2)
+    numer1 *= int(lcm/denom1) 
+    numer2 *= int(lcm/denom2)
+    return [numer1+numer2,lcm]
 
 def solution(numer1, denom1, numer2, denom2):
     for i in range(max(denom1,denom2),denom1*denom2+1,1):
@@ -52,3 +52,16 @@ def solution(numer1, denom1, numer2, denom2):
     return [numer1+numer2,lcm]
 
 print(solution(12,2,3,7))
+
+##### 8. 배열 두배 만들기
+def solution(numbers):
+    answer = []
+    for i in numbers:
+        answer.append(i*2)
+    
+    return answer
+
+##### 9. 나머지 구하기
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
