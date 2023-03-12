6bfcc25032f4b8a87fd3a5e46c18af352452a800
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-03-12 20:30:46 +0900

    [Bronze V] Title: A+B - 8, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,4 +1,5 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 for tc in range(1, n + 1):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     print(f'Case #{tc}: {a} + {b} = {a + b}')
