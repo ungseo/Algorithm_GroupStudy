6b605ce5de8de4af10de3fb10df8f4fa5fde7bfa
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-04-07 17:00:21 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 116724 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,34 @@
+import sys
+
+input =sys.stdin.readline
+
+n = int(input())
+stack = []
+for i in range(n):
+    cmd = list(input().split())
+    if cmd[0] == 'push':
+        stack.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if stack:
+            a = stack.pop(0)
+            print(a)
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+
+    elif cmd[0] == 'front':
+        if stack:
+            print(stack[0])
+        else:
+            print(-1)
+    else:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
