f2dd6e7bf0e4470a0c22db8e4047b303cff4d20c
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-04-08 21:37:07 +0900

    [Bronze III] Title: ACM 호텔, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -7,12 +7,17 @@ T = int(input())
 for tc in range(1, T + 1):
     H, W, N = map(int, input().split())
     cnt = 0
+    flag = 0
     for ho in range(1, W + 1):
         for F in range(1, H + 1):
             cnt += 1
             if cnt == N:
-                print(F,end='')
+                print(F, end='')
                 if ho < 10:
-                    print('0'+str(ho))
+                    print('0' + str(ho))
                 else:
                     print(ho)
+                flag = 1
+                break
+        if flag == 1:
+            break
