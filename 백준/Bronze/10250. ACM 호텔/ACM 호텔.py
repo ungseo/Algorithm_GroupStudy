42df62a59cb8da8c6873519ee9faad31f8223c1b
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-04-08 21:35:49 +0900

    [Bronze III] Title: ACM 호텔, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    H, W, N = map(int, input().split())
+    cnt = 0
+    for ho in range(1, W + 1):
+        for F in range(1, H + 1):
+            cnt += 1
+            if cnt == N:
+                print(F,end='')
+                if ho < 10:
+                    print('0'+str(ho))
+                else:
+                    print(ho)
