08ec81fac582d165e938f8bc54695158c6d4c356
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-04-08 22:58:25 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 
 L = int(input())
@@ -8,5 +9,5 @@ M = 1234567891
 hashsum = 0
 for i in range(L):
     hashsum += (ord(st[i]) - 96) * r ** i
-print(hashsum)
 
+print(hashsum % M)
