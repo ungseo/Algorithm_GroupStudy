b149e255e8d951586e567d2cdd5f7424d1c92377
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-04-12 10:20:43 +0900

    [Silver I] Title: 회의실 배정, Time: 448 ms, Memory: 128236 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+lst = []
+for i in range(n):
+    a, b = map(int, input().rstrip().split())
+    lst.append((a, b))
+
+lst.sort(key=lambda x:x[0])
+lst.sort(key=lambda x:x[1])
+cnt = 1
+dft = lst[0][1]
+for i in range(1,n):
+    if dft <= lst[i][0]:
+        dft = lst[i][1]
+        cnt += 1
+
+print(cnt)
