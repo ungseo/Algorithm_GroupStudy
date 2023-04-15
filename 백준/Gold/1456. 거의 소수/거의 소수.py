54c97007121a76aeaf755e313f5cb01349da15ca
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-04-15 21:23:48 +0900

    [Gold V] Title: 거의 소수, Time: 5168 ms, Memory: 193132 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+M = B ** 0.5
+
+prime = []
+num = [1] * (int(M) + 1)
+cnt = 0
+for i in range(2, int(B ** 0.25) + 1):
+    prime.append(i)
+    for j in range(i + i, int(M) + 1, i):
+        num[j] = 0
+cnt = 0
+for i in range(2,len(num)):
+    if num[i] == 1:
+        n = 2
+        while 1:
+            a = i ** n
+            if a < A:
+                n += 1
+                continue
+            if a > B:
+                break
+            n += 1
+            cnt += 1
+
+print(cnt)
