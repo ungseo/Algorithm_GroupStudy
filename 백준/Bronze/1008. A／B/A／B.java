8986135580a04663b81f9d064c091bb2fe1b74b1
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-05-15 14:42:39 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+       double a, b;
+       Scanner input = new Scanner(System.in);
+       a = input.nextDouble();
+       b = input.nextDouble();
+       System.out.println(a/b);
+    }
+}
