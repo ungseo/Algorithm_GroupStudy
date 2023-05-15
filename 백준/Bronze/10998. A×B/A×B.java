c2530311c3d9bbdb76cd6dc41f40b5cc22a44ffb
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-05-15 14:36:17 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.println(a * b);
+
+    }
+}
