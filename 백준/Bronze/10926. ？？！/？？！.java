c13c82d009bb9425a613a92b7168804914127271
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-05-15 14:51:17 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        String a;
+        Scanner input = new Scanner(System.in);
+        a = input.next();
+        System.out.println(a+"??!");
+        
+    }
+}
