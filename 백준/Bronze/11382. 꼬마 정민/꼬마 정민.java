8ecb95333c393cf732bf037f7d9ebf89374042dd
UngSeo <122588625+ungseo@users.noreply.github.com>
2023-05-15 15:22:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        long A, B, C;
+        Scanner input = new Scanner(System.in);
+        A = input.nextLong();
+        B = input.nextLong();
+        C = input.nextLong();
+
+        System.out.println(A+B+C);
+
+    }
+}
