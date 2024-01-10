4a1fba6e305ab5776eb7ec7310670ad2f8eec2a5
UngSeo <122588625+ungseo@users.noreply.github.com>
2024-01-10 17:48:25 +0900

    [level 2] Title: 호텔 대실, Time: 43.09 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,31 @@
+def solution(book_time):
+    answer = 0
+    bt = []
+    for i in book_time:
+        Sh, Sm = map(int, i[0].split(":"))
+        Eh, Em = map(int, i[1].split(":"))
+        
+        St = Sh * 60 + Sm
+        Et = Eh * 60 + Em
+        
+        bt.append([St, Et + 10])
+    bt.sort(key = lambda x:x[1])
+    
+    room = [bt[0][1]]
+    cnt = 1
+    print(room)
+    for i in range(1, len(bt)):
+        min_HT = 21e8
+        min_idx = 9999
+        for j in range(len(room)):
+            if bt[i][0] >= room[j] and min_HT > (bt[i][0] - room[j]):
+                min_HT = bt[i][0] - room[j]
+                
+                min_idx = j
+        if min_HT == 21e8 and min_idx == 9999:
+            room.append(bt[i][1])
+            cnt += 1
+        else:
+            room[min_idx] = bt[i][1]
+    return cnt
+
